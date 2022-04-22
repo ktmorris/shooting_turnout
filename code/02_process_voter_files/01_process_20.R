6611e4efcacfbbabfb613ac14e396f5e3dbce16e
Kevin Morris <morrisk@brennan.law.nyu.edu>
2022-04-22 17:15:15 -0400

    switch back to old neighborhood dfns

--- code/02_process_voter_files/01_process_20.R
+++ code/02_process_voter_files/01_process_20.R
@@ -1,10 +1,10 @@
 ## these functions assume that the raw L2 post-2020 voter files are in SQL databases 
 ## (one for file, one for history)
 
-db <- dbConnect(SQLite(), "D:/national_file_post20.db")
+db <- dbConnect(SQLite(), "M:/Democracy & Justice/democracy/voter_file_data/national_file_post20.db")
 tabs <- dbListTables(db)
 
-db_hist <- dbConnect(SQLite(), "D:/national_file_post20_history.db")
+db_hist <- dbConnect(SQLite(), "M:/Democracy & Justice/democracy/voter_file_data/national_file_post20_history.db")
 
 bg_ballots <- lapply(tabs, function(s){
   if(file.exists(paste0("temp/ballots_by_bg_", s, ".rds"))){
@@ -14,23 +14,17 @@ bg_ballots <- lapply(tabs, function(s){
     code_good <- unique(filter(fips_codes, state == s)$state_code)
     ## pull data
     tt <- dbGetQuery(db, paste0("select LALVOTERID,
-                                         Residence_Addresses_Latitude,
-                                         Residence_Addresses_Longitude from [", s, "]")) %>% 
-      mutate(across(starts_with("Residence"), as.numeric)) %>% 
-      filter(!is.na(Residence_Addresses_Longitude))
-    
-    bgs <- block_groups(state = s, year = 2020, class = "sp")
-    
-    pings  <- SpatialPoints(tt[,c('Residence_Addresses_Longitude','Residence_Addresses_Latitude')],
-                            proj4string = bgs@proj4string)
-    
-    tt$GEOID <- over(pings, bgs)$GEOID
-    
-    tt <- select(tt, LALVOTERID, GEOID)
-      
+                                         Residence_Addresses_CensusTract,
+                                         Residence_Addresses_CensusBlockGroup,
+                                         Voters_FIPS from [", s, "]")) %>%
+      ## take separate county, tract, and block group codes and combine to single GEOID
+      mutate(GEOID = paste0(code_good, str_pad(Voters_FIPS, width = 3, side = "left", pad = "0"),
+                            str_pad(Residence_Addresses_CensusTract, width = 6, side = "left", pad = "0"),
+                            Residence_Addresses_CensusBlockGroup)) %>%
+      select(LALVOTERID, GEOID)
     
     ## pull voter history data
-    hist <- dbGetQuery(db_hist, paste0("select * from ", s, "_history_20")) %>% 
+    hist <- dbGetQuery(db_hist, paste0("select LALVOTERID, General_2020_11_03 from ", s, "_history_20")) %>% 
       mutate(across(starts_with("General"), ~ ifelse(is.na(.) | . == "", "N", .)))
     
     ## merge file and history
