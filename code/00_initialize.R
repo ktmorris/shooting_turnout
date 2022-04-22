4578226d7289dc7b5566e37ac82cde9f41742f56
Kevin Morris <morrisk@brennan.law.nyu.edu>
2022-04-22 17:14:37 -0400

    add theme here

--- code/00_initialize.R
+++ code/00_initialize.R
@@ -34,8 +34,29 @@ library(data.table)
 ### this is a quick function that allows us to clear memory without losing things we
 ### want to keep from one script to the next
 
-save <- c("db", "cleanup", "theme_bc", "save", "weighted.ttest.ci")
 options("modelsummary_format_numeric_latex" = "plain")
+
+theme_bc <- function(base_size = 11, base_family = "BentonSans",
+                     legend.position = "right", face = "plain", ...) {
+  library(extrafont)
+  half_line <- base_size/2
+  theme_bw(base_family = base_family) %+replace%
+    theme(plot.caption = element_text(size = rel(0.8), hjust = 0,
+                                      family = base_family,
+                                      vjust = 1, margin = margin(t = half_line)),
+          plot.title = element_text(size = rel(1.2), hjust = 0.5,
+                                    vjust = 1, margin = margin(b = half_line),
+                                    family = base_family),
+          plot.subtitle = element_text(hjust = 0.5, vjust = 1, margin = margin(b = half_line)),
+          legend.position = legend.position,
+          text = element_text(family = base_family, face = face,
+                              colour = "black", size = base_size, lineheight = 0.9,
+                              hjust = 0.5, vjust = 0.5, angle = 0, margin = margin(),
+                              debug = FALSE),
+          ...)
+}
+
+save <- c("db", "cleanup", "theme_bc", "save", "weighted.ttest.ci")
 
 cleanup <- function(...){
   save2 <- c(save, ...)
