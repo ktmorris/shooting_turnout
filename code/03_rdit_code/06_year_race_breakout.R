73626677d1e01f50f6988c3aff94e40db73512d2
Kevin Morris <morrisk@brennan.law.nyu.edu>
2022-05-02 12:05:13 -0400

    write

---
+++ code/03_rdit_code/06_year_race_breakout.R
@@ -0,0 +1,313 @@
+
+### read in data saved previously
+dists <- readRDS("temp/shooting_demos.rds") %>% 
+  filter((date >= "2016-09-08" & date <= "2017-01-08") |
+           (date >= "2020-09-03" & date <= "2021-01-03"))
+
+dists <- left_join(dists, readRDS("temp/geocoded_shootings.rds") %>% 
+                     ungroup() %>% 
+                     mutate(id = row_number(),
+                            score = ifelse(is.na(score), 100, as.numeric(score))) %>% 
+                     filter(score > 95) %>% 
+                     select(id, race)) %>% 
+  mutate(across(starts_with("race"), ~substring(., 1, 1)),
+         across(starts_with("race"), ~ifelse(. == "", "U",
+                                             ifelse(. %in% c("A", "N", "P"), "O", .))),
+         so = 1 - nh_white - nh_black - latino,
+         plu = ifelse(nh_white > nh_black & nh_white > latino & nh_white > so, "w",
+                      ifelse(nh_black > nh_white & nh_black > latino & nh_black > so, "b",
+                             ifelse(latino > nh_white & latino > nh_black & latino > so, "l", "o"))))
+
+###################################################################
+###################################################################
+###################################################################
+###################################################################
+###################################################################
+###################################################################
+###################################################################
+## loop over thresholds for different plurality neighborhoods
+
+## the logic in this file follows that of the previous one; less commenting is hence given here
+
+out <- rbindlist(lapply(c("b", "l", "w"), function(pl){
+  rbindlist(lapply(c(seq(0.35, 1, 0.05), seq(2, 20, 1)), function(threshold){
+    
+    ##keep the observations within the threshold closest to election day
+    ## using data.table notation to speed this up
+    set_pre <- dists[dists$dist <= threshold & dists$pre & dists$plu == pl,
+                     .SD[date %in% max(date)], by=list(year, GEOID)]
+    set_pre <- set_pre[, .SD[1], by = .(year, GEOID)] %>% 
+      mutate(treated = T)
+    
+    ##keep the observations within the threshold closest to election day
+    set_post <- dists[dists$dist <= threshold & !dists$pre & dists$plu == pl,
+                      .SD[date %in% min(date)], by=list(year, GEOID)]
+    set_post <- set_post[!(paste0(set_post$GEOID, set_post$year) %in% paste0(set_pre$GEOID, set_pre$year)),
+                         .SD[1], by = .(year, GEOID)] %>% 
+      mutate(treated = F)
+    rbindlist(lapply(c(2016, 2020), function(y){
+      print(paste(y, pl, threshold))
+      full_treat <- bind_rows(set_pre, set_post) %>% 
+        select(GEOID, id, date, dist, year, turnout,
+               median_income, nh_white, nh_black, median_age, pop_dens, latino, asian,
+               some_college, turnout_pre, treated) %>% 
+        mutate(d2 = ifelse(year == "2020", as.integer(date - as.Date("2020-11-03")),
+                           as.integer(date - as.Date("2016-11-08")))) %>% 
+        filter(year == y)
+      
+      full_treat <- full_treat[complete.cases(select(full_treat,
+                                                     latino, nh_white, asian, nh_black, median_income, median_age,
+                                                     pop_dens, turnout_pre, some_college)), ]
+      
+      ########################################
+      
+      l <- try(rdrobust(y = full_treat$turnout, x = full_treat$d2, p = 1, c = 0, cluster = full_treat$id))
+      
+      l2 <- try(rdrobust(y = full_treat$turnout, x = full_treat$d2, p = 1, c = 0, cluster = full_treat$id,
+                         covs = select(full_treat,
+                                       latino, nh_white, asian,
+                                       nh_black, median_income, median_age,
+                                       pop_dens, turnout_pre, some_college)))
+      
+      if(inherits(l, "try-error")){
+        f <- tibble(coef = double(),
+                    se = double(), 
+                    pv = double(),
+                    p = threshold,
+                    n = double(),
+                    bwidth = double(),
+                    u = double(),
+                    l = double(),
+                    t = "no_covs")
+      }else{
+        f <- tibble(coef = l$coef,
+                    se = l$se, 
+                    pv = l$pv,
+                    p = threshold,
+                    n = l$N_h[1] + l$N_h[2],
+                    bwidth = l[["bws"]][1],
+                    u = l[["ci"]][,2],
+                    l = l[["ci"]][,1],
+                    t = "no_covs")
+      }
+      
+      if(inherits(l2, "try-error")){
+        f <- bind_rows(f,
+                       tibble(coef = double(),
+                              se = double(), 
+                              pv = double(),
+                              p = threshold,
+                              n = double(),
+                              bwidth = double(),
+                              u = double(),
+                              l = double(),
+                              t = "covs"))
+      }else{
+        f <- bind_rows(f, 
+                       tibble(coef = l2$coef,
+                              se = l2$se, 
+                              pv = l2$pv,
+                              p = threshold,
+                              n = l2$N_h[1] + l2$N_h[2],
+                              bwidth = l2[["bws"]][1],
+                              u = l2[["ci"]][,2],
+                              l = l2[["ci"]][,1],
+                              t = "covs"))
+      }
+      
+      return(mutate(f, year = y))
+    }))
+  })) %>% 
+    mutate(bw = pl)
+}))
+
+saveRDS(out, "temp/plu_data_years.rds")
+out <- readRDS("temp/plu_data_years.rds")
+
+
+out$estimate <- rep(c('Traditional','Bias-Adjusted','Robust'),nrow(out)/3)
+
+out <- mutate_at(out, vars(coef, l, u), ~. * -1)
+out <- filter(out, estimate == "Robust", t == "covs")
+
+out <- mutate(out,
+              bw = ifelse(bw == "b", "Plurality Black",
+                          ifelse(bw == "l", "Plurality Latinx",
+                                 "Plurality White")))
+# Create Figure 4A 
+different_dists1 <- ggplot(filter(out, p <= 1, p > 0.31),
+                           aes(x = p, y = coef, ymin = l, ymax = u)) +
+  facet_grid(year~bw) +
+  geom_point() +
+  geom_errorbar(width = 0) + 
+  theme_bc(base_family = "LM Roman 10") +
+  theme(plot.title = element_text(size = 12)) +
+  geom_hline(yintercept = 0, linetype = "dashed") +
+  labs(y = "LATE", x = "Radius Around Shooting (Miles)") +
+  ggtitle("(a) By Block Group Racial Composition") +
+  coord_cartesian(ylim = c(-1, 1))
+different_dists1
+
+different_dists2 <- ggplot(filter(out, p >= 1, bw == "Plurality Black"),
+                           aes(x = p, y = coef, ymin = l, ymax = u)) +
+  geom_point() +
+  facet_grid(year~.) +
+  geom_errorbar(width = 0) + 
+  theme_bc(base_family = "LM Roman 10") +
+  geom_hline(yintercept = 0, linetype = "dashed") +
+  theme(plot.title = element_text(size = 12)) +
+  labs(y = "LATE", x = "Radius Around Shooting (Miles)") +
+  scale_x_continuous(breaks = seq(2, 20, 2)) +
+  ggtitle("(b) Extended Distance,
+Plurality-Black Block Groups")
+
+different_dists2
+
+###################################################################
+###################################################################
+###################################################################
+##
+## the logic in this file follows that of the previous one; less commenting is hence given here
+
+out <- rbindlist(lapply(c("B", "H", "W"), function(r){
+  rbindlist(lapply(c(seq(0.25, 1, 0.05)), function(threshold){
+    
+    ##keep the observations within the threshold closest to election day
+    ## using data.table notation to speed this up
+    set_pre <- dists[dists$dist <= threshold & dists$pre & dists$race == r,
+                     .SD[date %in% max(date)], by=list(year, GEOID)]
+    set_pre <- set_pre[, .SD[1], by = .(year, GEOID)] %>% 
+      mutate(treated = T)
+    
+    ##keep the observations within the threshold closest to election day
+    set_post <- dists[dists$dist <= threshold & !dists$pre & dists$race == r,
+                      .SD[date %in% min(date)], by=list(year, GEOID)]
+    set_post <- set_post[!(paste0(set_post$GEOID, set_post$year) %in% paste0(set_pre$GEOID, set_pre$year)),
+                         .SD[1], by = .(year, GEOID)] %>% 
+      mutate(treated = F)
+    rbindlist(lapply(c(2016, 2020), function(y){
+      print(paste(y, r, threshold))
+      
+      full_treat <- bind_rows(set_pre, set_post) %>% 
+        select(GEOID, id, date, dist, year, turnout,
+               median_income, nh_white, nh_black, median_age, pop_dens, latino, asian,
+               some_college, turnout_pre, treated) %>% 
+        mutate(d2 = ifelse(year == "2020", as.integer(date - as.Date("2020-11-03")),
+                           as.integer(date - as.Date("2016-11-08")))) %>% 
+        filter(year == y)
+      
+      full_treat <- full_treat[complete.cases(select(full_treat,
+                                                     latino, nh_white, asian, nh_black, median_income, median_age,
+                                                     pop_dens, turnout_pre, some_college)), ]
+      
+      ########################################
+      
+      l <- try(rdrobust(y = full_treat$turnout, x = full_treat$d2, p = 1, c = 0, cluster = full_treat$id, h = 12))
+      
+      l2 <- try(rdrobust(y = full_treat$turnout, x = full_treat$d2, p = 1, c = 0, cluster = full_treat$id,
+                         covs = select(full_treat,
+                                       latino, nh_white, asian,
+                                       nh_black, median_income, median_age,
+                                       pop_dens, turnout_pre, some_college)))
+      if(inherits(l2, "try-error")){
+        l2 <- try(rdrobust(y = full_treat$turnout, x = full_treat$d2, p = 1, c = 0, cluster = full_treat$id, h = 10,
+                           covs = select(full_treat,
+                                         latino, nh_white, asian,
+                                         nh_black, median_income, median_age,
+                                         pop_dens, turnout_pre, some_college)))
+      }
+      
+      if(inherits(l, "try-error")){
+        f <- tibble(coef = double(),
+                    se = double(), 
+                    pv = double(),
+                    p = threshold,
+                    n = double(),
+                    bwidth = double(),
+                    u = double(),
+                    l = double(),
+                    t = "no_covs")
+      }else{
+        f <- tibble(coef = l$coef,
+                    se = l$se, 
+                    pv = l$pv,
+                    p = threshold,
+                    n = l$N_h[1] + l$N_h[2],
+                    bwidth = l[["bws"]][1],
+                    u = l[["ci"]][,2],
+                    l = l[["ci"]][,1],
+                    t = "no_covs")
+      }
+      
+      if(inherits(l2, "try-error")){
+        f <- bind_rows(f,
+                       tibble(coef = double(),
+                              se = double(), 
+                              pv = double(),
+                              p = threshold,
+                              n = double(),
+                              bwidth = double(),
+                              u = double(),
+                              l = double(),
+                              t = "covs"))
+      }else{
+        f <- bind_rows(f, 
+                       tibble(coef = l2$coef,
+                              se = l2$se, 
+                              pv = l2$pv,
+                              p = threshold,
+                              n = l2$N_h[1] + l2$N_h[2],
+                              bwidth = l2[["bws"]][1],
+                              u = l2[["ci"]][,2],
+                              l = l2[["ci"]][,1],
+                              t = "covs"))
+      }
+      
+      return(mutate(f, year = y))
+    }))
+    
+    
+  })) %>% 
+    mutate(bw = r)
+}))
+
+saveRDS(out, "temp/race_data_years.rds")
+out <- readRDS("temp/race_data_years.rds")
+
+
+out$estimate <- rep(c('Traditional','Bias-Adjusted','Robust'),nrow(out)/3)
+
+out <- mutate_at(out, vars(coef, l, u), ~. * -1)
+out <- filter(out, estimate == "Robust", t == "covs")
+
+out <- mutate(out,
+              bw = ifelse(bw == "B", "Black Victim",
+                          ifelse(bw == "H", "Latinx Victim",
+                                 "White Victim")))
+#create figure 4C 
+different_dists3 <- ggplot(filter(out, p <= 1, p > 0.31),
+                           aes(x = p, y = coef, ymin = l, ymax = u)) +
+  facet_grid(year~bw) +
+  geom_point() +
+  geom_errorbar(width = 0) + 
+  theme_bc(base_family = "LM Roman 10") +
+  theme(plot.title = element_text(size = 12)) +
+  geom_hline(yintercept = 0, linetype = "dashed") +
+  labs(y = "LATE", x = "Radius Around Shooting (Miles)") +
+  ggtitle("(c) By Victim Race")
+different_dists3
+
+## create figure 4D
+different_dists4 <- ggplot(filter(out, p >= 1, bw == "Black Victim"),
+                           aes(x = p, y = coef, ymin = l, ymax = u)) +
+  geom_point() +
+  facet_grid(year~.) +
+  geom_errorbar(width = 0) + 
+  theme_bc(base_family = "LM Roman 10") +
+  theme(plot.title = element_text(size = 12)) +
+  geom_hline(yintercept = 0, linetype = "dashed") +
+  labs(y = "LATE", x = "Radius Around Shooting (Miles)") +
+  scale_x_continuous(breaks = seq(2, 20, 2)) +
+  ggtitle("(d) Extended Distance,
+Black Victims")
+different_dists4
